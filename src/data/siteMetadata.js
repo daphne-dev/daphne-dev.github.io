e2a4d3eb0c026cd442f257b352d4d5b54aa8dda8
Daphne-dev <daphned@naver.com>
2024-01-04 15:46:44 +0900

    giscus 댓글 추가

--- src/data/siteMetadata.js
+++ src/data/siteMetadata.js
@@ -51,10 +51,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'Daphne-dev/daphne-dev.github.io',
+      repositoryId: 'R_kgDOLAbwOw',
+      category: 'General',
+      categoryId: 'DIC_kwDOLAbwO84CcLy2',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
@@ -69,7 +69,7 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
       // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'en',
+      lang: 'ko',
     },
   },
   search: {
